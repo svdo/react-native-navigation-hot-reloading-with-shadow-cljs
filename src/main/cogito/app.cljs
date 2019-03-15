0d595e11282679da32e5b7b205bcd19b58c6d1b2
Thomas Heller <th.heller@gmail.com>
2019-03-15 20:01:51 +0100

    working hot-reload and overkill refactor

--- src/main/cogito/app.cljs
+++ src/main/cogito/app.cljs
@@ -1,60 +1,25 @@
 (ns cogito.app
   (:require
-   [reagent.core :as r :refer [atom]]
-   ["react-native" :as ReactNative]
-   ["react-native-navigation" :as ReactNativeNavigation :refer (Navigation)]
-   ["create-react-class" :as crc]
-   [cogito.home :as home]
-   [cogito.identity-manager :as identity-manager]
-   [cogito.create-identity :as create-identity]))
+    [reagent.core :as r :refer [atom]]
+    ["react-native" :as ReactNative]
+    ["react-native-navigation" :as rnn]
+    [cogito.env :as env]
+    [cogito.home :as home]
+    [cogito.identity-manager :as identity-manager]
+    [cogito.create-identity :as create-identity]))
 
-(defonce root-ref (atom nil))
-(defonce reload-wrapper-ref (atom nil))
 
-(defn reload-wrapper [component-name root]
-  (let [first-call? (nil? @root-ref)]
-    (println "reload wrapper ref" @reload-wrapper-ref root)
-    (reset! root-ref (r/as-element [root]))
-    (if-not first-call?
-      (when-let [wrapper @reload-wrapper-ref]
-        (println "!!! force update !!!")
-        (.forceUpdate wrapper))
-      (let [wrapper (crc #js
-                          {:displayName "ReloadWrapper"
+(defn init []
+  (env/register "Home")
+  (env/register "IdentityManager")
+  (env/register "CreateIdentity")
 
-                           :componentDidMount
-                           (fn []
-                             (println "wrapper did mount")
-                             (this-as this (reset! reload-wrapper-ref this)))
-
-                           :componentWillUnmount
-                           (fn [] (reset! reload-wrapper-ref nil))
-
-                           :render
-                           (fn []
-                             (this-as this
-                                      (js/console.log (.-props this) @root-ref)
-                                      (let [body @root-ref]
-                                        body)))})] ;; todo pass props to body somehow
-
-        (.registerComponent Navigation
-                            component-name
-                            (fn [] wrapper)
-                            #(r/reactify-component root))
-        (.registerComponent Navigation "IdentityManager" identity-manager/screen)
-        (.registerComponent Navigation "CreateIdentity" create-identity/screen)))))
-
-(defn init {:dev/after-load true} []
-  (reload-wrapper "Home" home/screen)
-
-  (let [events (.events Navigation)]
-    (.registerAppLaunchedListener
-     events
-     #(.setRoot
-       Navigation
-       #js {:root #js
-                   {:stack #js
-                            {:children #js [#js
-                                             {:component #js
-                                                          {:name "Home"}}]
-                             :options #js {:topBar #js {:visible "false"}}}}}))))
+  (-> (rnn/Navigation.events)
+      (.registerAppLaunchedListener
+        (fn []
+          (->> {:root
+                {:stack
+                 {:children [{:component {:name "Home"}}]
+                  :options {:topBar {:visible "false"}}}}}
+               (clj->js)
+               (rnn/Navigation.setRoot))))))
--- src/main/cogito/toolbar_button.cljs
+++ src/main/cogito/toolbar_button.cljs
@@ -1,13 +1,11 @@
 (ns cogito.toolbar-button
   (:require
-   ["react-native" :as ReactNative]))
-
-(def platform (.-Platform ReactNative))
+   ["react-native" :as rn]))
 
 (defn toolbar-button-android [id] {:id id :text id})
 (defn toolbar-button-ios [id] {:id id :systemItem id})
 
 (defn toolbar-button [id]
-  (if (= "ios" (.-OS platform))
+  (if (= "ios" rn/Platform.OS)
     (toolbar-button-ios id)
     (toolbar-button-android id)))
